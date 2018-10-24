fc865d6e2af0b16336a277bea1ac2518e6bfdde0
WeatherBaren <baohailong@sina.com>
2018-10-24 08:49:51 +0800

    fix package

--- src/main/java/com/bhl/ehotel/ConfigServer.java
+++ src/main/java/com/bhl/ehotel/ConfigServer.java
@@ -1,4 +1,4 @@
-package com.bhl.ehotel.config_server;
+package com.bhl.ehotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
